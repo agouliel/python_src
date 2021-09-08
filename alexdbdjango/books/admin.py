32dc284f1d437bdc91fcdaff9b40ed5b5bc55556
agouliel_imac <agouliel@gmail.com>
2021-09-08 14:37:00 +0300

    added locs and categs

--- alexdbdjango/books/admin.py
+++ alexdbdjango/books/admin.py
@@ -3,8 +3,10 @@ from django.contrib import admin
 from .models import Tblbook, Tblbookscateg, Tblbookslocations
 
 class BookAdmin(admin.ModelAdmin):
-  list_display = ('title', 'author', 'publisher')
-  list_filter = ['category', 'location']
-  search_fields = ['title']
+  list_display = ('title', 'author', 'translator', 'publisher', 'location')
+  list_filter = ['category']
+  search_fields = ['title', 'author']
 
 admin.site.register(Tblbook, BookAdmin)
+admin.site.register(Tblbookscateg)
+admin.site.register(Tblbookslocations)
