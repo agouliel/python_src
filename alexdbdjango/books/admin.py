8f5ab48d91a64ed499bf31006afd233000169a86
agouliel_imac <agouliel@gmail.com>
2021-09-08 16:25:27 +0300

    fixed textbox size

--- alexdbdjango/books/admin.py
+++ alexdbdjango/books/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.forms import Textarea #TextInput
+from django.db import models
 
 from .models import Tblbook, Tblbookscateg, Tblbookslocations
 
@@ -6,7 +8,21 @@ class BookAdmin(admin.ModelAdmin):
   list_display = ('title', 'author', 'translator', 'publisher', 'location')
   list_filter = ['category']
   search_fields = ['title', 'author']
+  formfield_overrides = {
+        #models.CharField: {'widget': TextInput(attrs={'size':'20'})},
+        models.TextField: {'widget': Textarea(attrs={'rows':2, 'cols':80})},
+  }
+
+class CategAdmin(admin.ModelAdmin):
+  formfield_overrides = {
+        models.TextField: {'widget': Textarea(attrs={'rows':2, 'cols':80})},
+  }
+
+class LocationAdmin(admin.ModelAdmin):
+  formfield_overrides = {
+        models.TextField: {'widget': Textarea(attrs={'rows':2, 'cols':80})},
+  }
 
 admin.site.register(Tblbook, BookAdmin)
-admin.site.register(Tblbookscateg)
-admin.site.register(Tblbookslocations)
+admin.site.register(Tblbookscateg, CategAdmin)
+admin.site.register(Tblbookslocations, LocationAdmin)
