003b2638c85fe3c547ccedc71a6b69fb1c5311b0
agouliel <agouliel@gmail.com>
2021-09-13 20:50:18 +0300

    added songs

--- alexdbdjango/alexdbdjango/settings.py
+++ alexdbdjango/alexdbdjango/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'books.apps.BooksConfig',
+    'songs.apps.SongsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
---
+++ alexdbdjango/songs/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+
+from django.forms import Textarea #TextInput
+from django.db import models
+
+from .models import Tblsongs
+
+class SongAdmin(admin.ModelAdmin):
+  list_display = ('title', 'artist', 'album', 'composer')
+  list_filter = ['composer']
+  search_fields = ['title', 'artist', 'album']
+  formfield_overrides = {
+        #models.CharField: {'widget': TextInput(attrs={'size':'20'})},
+        models.TextField: {'widget': Textarea(attrs={'rows':2, 'cols':80})},
+  }
+
+admin.site.register(Tblsongs, SongAdmin)
