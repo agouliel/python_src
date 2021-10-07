8ae6ad25eb06336f6d7d14980518e861e61378c6
agouliel_imac <agouliel@gmail.com>
2021-10-07 17:37:58 +0300

    added location

---
+++ loc.py
@@ -0,0 +1,32 @@
+# https://www.youtube.com/watch?v=Dz3rSZHnKkM
+
+import sys, os
+
+import phonenumbers
+from phonenumbers import carrier
+from phonenumbers import geocoder
+
+from opencage.geocoder import OpenCageGeocode
+
+import folium
+
+mynumber = '+30'+sys.argv[1]
+
+# find country
+pepnumber = phonenumbers.parse(mynumber)
+#print(carrier.name_for_number(pepnumber, "en"))
+location = phonenumbers.geocoder.description_for_number(pepnumber, "en")
+print(location)
+
+# get lat and long from country
+key = os.environ['OPENCAGEKEY']
+geoc = OpenCageGeocode(key)
+results = geoc.geocode(str(location))
+lat = results[0]['geometry']['lat']
+lng = results[0]['geometry']['lng']
+print(lat, lng)
+
+# make map from lat and long
+myMap = folium.Map(location=[lat,lng], zoom_start=9)
+folium.Marker([lat,lng], popup=location).add_to(myMap)
+myMap.save("mylocation.html")
