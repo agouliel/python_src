09c1aa352323f5e6a84b2c1969ac6c160935eb8c
agouliel_imac <agouliel@gmail.com>
2021-11-29 11:59:57 +0200

    updated coinbase

--- crypto_py/coinbase/coinb.py
+++ crypto_py/coinbase/coinb.py
@@ -5,9 +5,28 @@ from coinbase.wallet.client import Client
 
 client = Client(os.environ['COINKEY'], os.environ['COINSECR'])
 
+#################
+
+client.get_exchange_rates()
+client.get_buy_price(currency_pair = 'BTC-USD')
+client.get_sell_price(currency_pair = 'BTC-USD')
+client.get_spot_price(currency_pair = 'BTC-USD')
+
+#################
+
 user = client.get_current_user()
 print(user['name'])
-print(user['email'])
 
-#balance = coinbase.get_balance()
-#print('Balance is ' + balance)
+#################
+
+accounts = client.get_accounts()
+print(accounts[0])
+
+account = client.get_primary_account()
+print(account)
+
+account.get_addresses()
+account.get_transactions()
+#tx = account.send_money(to='test@test.com', amount='1', currency='BTC', two_factor_token="123456")
+
+client.get_payment_methods()
