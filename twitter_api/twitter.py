ed9c2002eea2b9af995f1cf62621b864f51a4094
agouliel <agouliel@gmail.com>
2021-12-05 13:16:36 +0200

    added twitter

---
+++ twitter_api/twitter.py
@@ -0,0 +1,14 @@
+from twython import Twython
+import os
+
+CONSUMER_KEY = os.environ['T_CONS_KEY']
+CONSUMER_SECRET = os.environ['T_CONS_SECRET']
+ACCESS_TOKEN = os.environ['T_ACCESS_TOKEN']
+ACCESS_TOKEN_SECRET = os.environ['T_ACCESS_TOKEN_SECRET']
+
+twitter_client = Twython(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+
+for status in twitter_client.search(q='"data science"')["statuses"]:
+    user = status["user"]["screen_name"]
+    text = status["text"]
+    print(f"{user}: {text}\n")
