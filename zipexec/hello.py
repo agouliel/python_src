8cf3c78676cfa601373f9d8df49ddc8e320545be
agouliel <agouliel@gmail.com>
2022-03-26 13:02:00 +0200

    fixed zipexec

--- zipexec/hello.py
+++ zipexec/hello.py
@@ -1,1 +1,13 @@
+# https://twitter.com/simonw/status/1507470695298609153
+# https://bugs.python.org/issue1739468
+
+# header.txt:
+# #!/usr/bin/env python3
+
+# zip myzip.zip hello.py __main__.py
+# cat header.txt myzip.zip > exe.zip
+# chmod +x exe.zip                  
+# ./exe.zip
+
+
 print("hello")
