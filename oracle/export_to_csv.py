168990da220c1a7eb36b0d4a14a613f49a9ac34a
agouliel_imac <agouliel@gmail.com>
2022-05-31 16:07:23 +0300

    added oracle folder

---
+++ oracle/export_to_csv.py
@@ -0,0 +1,17 @@
+# https://www.oracle.com/database/technologies/appdev/python/quickstartpython.html
+
+import cx_Oracle
+import pandas as pd
+
+passw = input('Password: ')
+conn = cx_Oracle.connect(user="admin", password=passw, dsn="db202109301512_high")
+
+#cursor = conn.cursor()
+
+sql_query = pd.read_sql_query('''
+                              select * from tblsong
+                              '''
+                              ,conn)
+
+df = pd.DataFrame(sql_query)
+df.to_csv ('exported_data.csv', index = False)
