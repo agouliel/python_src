24a6b529ce3e93093e809a22784e16e43a84b355
agouliel <agouliel@gmail.com>
2022-11-06 23:52:21 +0200

    added server

---
+++ servertest.py
@@ -0,0 +1,29 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import time
+import requests
+
+URL = "https://raw.githubusercontent.com/agouliel/yamdl_test/main/myapp/templates/myapp/test.html"
+response = requests.get(URL)
+
+hostName = "localhost"
+serverPort = 8080
+
+class MyServer(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+
+        self.wfile.write(response.content)
+
+if __name__ == "__main__":        
+    webServer = HTTPServer((hostName, serverPort), MyServer)
+    print("Server started http://%s:%s" % (hostName, serverPort))
+
+    try:
+        webServer.serve_forever()
+    except KeyboardInterrupt:
+        pass
+
+    webServer.server_close()
+    print("Server stopped.")
