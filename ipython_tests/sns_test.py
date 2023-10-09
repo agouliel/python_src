a2e251f9449a8d41fbac4c462b5bf723d18d088e
agouliel <agouliel@gmail.com>
2023-10-09 19:16:43 +0300

    added deitel example

--- ipython_tests/sns_test.py
+++ ipython_tests/sns_test.py
@@ -2,6 +2,8 @@
 # %run ./sns_test.py
 
 import seaborn as sns
+import numpy as np
+import random
 
 sns.set_theme()
 tips = sns.load_dataset("tips")
@@ -10,3 +12,6 @@ sns.relplot(
   x="total_bill", y="tip", col="time",
   hue="smoker", style="smoker", size="size",
 )
+rolls = [random.randrange(1,7) for i in range(600)]
+values, frequencies = np.unique(rolls, return_counts=True)
+#axes = sns.barplot(x=values, y=frequencies, palette='bright')
