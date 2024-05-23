650571d00640f38cca16152eed8af0d6b9ecb8c7
agouliel_imac <agouliel@gmail.com>
2024-05-23 13:43:03 +0300

    added observations sire2 in clean_html_tags.py

--- clean_html_tags.py
+++ clean_html_tags.py
@@ -21,20 +21,19 @@ def clean_file(filedata):
 
     return filedata
 
-#db = 'AttendanceV1'
-db = 'Hephaestus'
+db = 'AttendanceV1' # 'Hephaestus'
 cnxn = pyodbc.connect(f"DRIVER={{ODBC Driver 17 for SQL Server}};SERVER=servsrv\mssql2016;DATABASE={db};UID={os.environ['SERVDBUSER']};PWD={os.environ['SERVDBPASS']}")
 cursor = cnxn.cursor()
 #stmt = "select ObjectId, comments from Vetting where vetid=6158 and comments is not null and comments<>''"
-stmt = "select ProblemResolutionStepId, Remarks from ProblemResolutionSteps where QualitySafetyEventId = '1B17C58D-12AD-4C16-93B0-C6140D7F05F8'"
+stmt = 'select objectid, obs_text from ObservationsSire2 where vetid = 6158'
+#stmt = "select ProblemResolutionStepId, Remarks from ProblemResolutionSteps where QualitySafetyEventId = '1B17C58D-12AD-4C16-93B0-C6140D7F05F8'"
 cursor.execute(stmt)
 results = cursor.fetchall()
 
 for row in results:
   #upd_stmt = f"""update vetting set comments='{clean_file(row[1]).replace("'", '')}' where vetid=6158 and objectid='{row[0]}'"""
-  upd_stmt = f"""update ProblemResolutionSteps
-  set Remarks = '{clean_file(row[1]).replace("'", '')}'
-  where ProblemResolutionStepId = '{row[0]}'"""
+  upd_stmt = f"""update ObservationsSire2 set obs_text='{clean_file(row[1]).replace("'", '')}' where vetid = 6158  and objectid='{row[0]}'"""
+  #upd_stmt = f"""update ProblemResolutionSteps set Remarks = '{clean_file(row[1]).replace("'", '')}' where ProblemResolutionStepId = '{row[0]}'"""
   #print(upd_stmt)
   #print('------------')
   cursor.execute(upd_stmt)
