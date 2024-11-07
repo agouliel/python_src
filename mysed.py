4e2e17a7dac6e94b3e019e79bc3f93347b232e92
agouliel <agouliel@gmail.com>
2024-11-07 12:48:52 +0200

    only consider html lines in mysed.py

--- mysed.py
+++ mysed.py
@@ -7,7 +7,7 @@ new_contents = []
 
 for line in contents:
   x = re.search('id="(.*)"', line)
-  if x:
+  if x and line.endswith('>\n'):
     print(line)
     text_inside = x.group(1) # https://stackoverflow.com/questions/9889635/regular-expression-to-return-all-characters-between-two-special-characters
     new_line = re.sub(f'id="{text_inside}"', f'id="{text_inside}" name="{text_inside}"', line)
