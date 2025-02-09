f55994e9b47cdd4fa22a182b50d2eb667a11c4a7
agouliel <agouliel@gmail.com>
2025-02-09 17:55:25 +0200

    results on same page in guyton

--- guyton/guyton_web.py
+++ guyton/guyton_web.py
@@ -6,15 +6,23 @@ app = Flask(__name__)
 @app.route('/calculator', methods=['GET','POST'])
 def calculator():
     if request.method == 'POST':
-        cvp = float(request.form['cvp'])
-        map = float(request.form['map'])
-        co = float(request.form['co'])
-        age = float(request.form['age'])
-        height = float(request.form['height'])
-        weight = float(request.form['weight'])
+        if (request.form['cvp'] and request.form['map'] and request.form['co']
+            and request.form['age'] and request.form['height'] and request.form['weight']):
+            cvp = float(request.form['cvp'])
+            map = float(request.form['map'])
+            co = float(request.form['co'])
+            age = float(request.form['age'])
+            height = float(request.form['height'])
+            weight = float(request.form['weight'])
 
-        result = pmsa(cvp, map, co, age, height, weight)
+            result = pmsa(cvp, map, co, age, height, weight)
 
-        return render_template('result.html', pmsa=result[0], pvr=result[1], eh=result[2], rvr=result[3], power=result[4], cpo=result[5])
+            return render_template('form.html', calculation_success=True,
+                                   cvp=cvp, map=map, co=co, age=int(age), height=int(height), weight=int(weight),
+                                   pmsa=result[0], pvr=result[1], eh=result[2], rvr=result[3], power=result[4], cpo=result[5])
+        else:
+            # https://medium.com/@alanbanks229/part-2-of-2-introduction-to-python-flask-29b58adbabaf
+            # https://github.com/alanbanks229/flask_calculator_app
+            return render_template('form.html', calculation_success=False, error='Please submit all values')
     
     return render_template('form.html')
