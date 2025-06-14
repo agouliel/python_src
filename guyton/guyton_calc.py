ce1abd6b72dfc5fa91612bad99dc109658698512
Alex Goulielmos <agouliel@gmail.com>
2025-06-14 23:53:36 +0300

    added before values and delta calculations

--- guyton/guyton_calc.py
+++ guyton/guyton_calc.py
@@ -4,13 +4,14 @@
 from openpyxl import load_workbook
 import csv
 
-def pmsa(cvp_rap, map_in, co, age, height, weight):
+def pmsa(cvp_rap, map_in, co, age, height, weight, cvpb=0, mapb=0, cob=0):
   a = 0.96
   b = 0.04
   c_nom = 0.96 * 0.038 * (94.17 + 0.193*age) # 0.96 is missing from the above link
   c_denom = 4.5 * (0.99**(age-15)) * 0.007184 * (height**0.725) * (weight**0.425)
   c = c_nom / c_denom
   pmsa = a*cvp_rap + b*map_in + c*co
+  pmsab = a*cvpb + b*mapb + c*cob
 
   pvr = pmsa - cvp_rap # this is also called VRdP
   eh = pvr/pmsa
@@ -19,7 +20,16 @@ def pmsa(cvp_rap, map_in, co, age, height, weight):
   power = co * (map_in - cvp_rap) * 0.0022
   cpo = (co * map_in) / 451
 
-  return (round(pmsa,2), round(pvr,2), round(eh,2), round(rvr,2), round(power,2), round(cpo,2))
+  if pmsa != pmsab:
+    epower = ((co * (map_in - cvp_rap)) - (cob * (mapb - cvpb))) * 0.0022 / (pmsa - pmsab)
+    evol = (pmsa - cvp_rap) - (pmsab - cvpb) / (pmsa - pmsab)
+  else:
+    epower = 0
+    evol = 0
+
+  return (round(pmsa,2), round(pvr,2), round(eh,2), round(rvr,2), round(power,2), round(cpo,2),
+    round(epower,2), round(evol,2),
+  )
 
 data = []
 data.append(['CVP', 'MAP', 'CO', 'AGE', 'HEIGHT', 'WEIGHT', 'PMSA', 'PVR', 'EH', 'RVR', 'POWER', 'CPO']) # header
--- guyton/guyton_web.py
+++ guyton/guyton_web.py
@@ -11,13 +11,25 @@ def calculator():
             cvp = float(request.form['cvp'])
             map = float(request.form['map'])
             co = float(request.form['co'])
+            if request.form['cvpb'] and request.form['mapb'] and request.form['cob']:
+                cvpb = float(request.form['cvpb'])
+                mapb = float(request.form['mapb'])
+                cob = float(request.form['cob'])
             age = float(request.form['age'])
             height = float(request.form['height'])
             weight = float(request.form['weight'])
 
-            result = pmsa(cvp, map, co, age, height, weight)
-
-            return render_template('form.html', calculation_success=True,
+            if request.form['cvpb'] and request.form['mapb'] and request.form['cob']:
+                result = pmsa(cvp, map, co, age, height, weight, cvpb, mapb, cob)
+                return render_template('form.html', calculation_success=True,
+                                   cvp=cvp, map=map, co=co, age=int(age), height=int(height), weight=int(weight),
+                                   cvpb=cvpb, mapb=mapb, cob=cob,
+                                   pmsa=result[0], pvr=result[1], eh=result[2], rvr=result[3], power=result[4], cpo=result[5],
+                                   epower=result[6], evol=result[7],
+                                   )
+            else:
+                result = pmsa(cvp, map, co, age, height, weight)
+                return render_template('form.html', calculation_success=True,
                                    cvp=cvp, map=map, co=co, age=int(age), height=int(height), weight=int(weight),
                                    pmsa=result[0], pvr=result[1], eh=result[2], rvr=result[3], power=result[4], cpo=result[5])
         else:
