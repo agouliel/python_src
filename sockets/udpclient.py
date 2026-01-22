a1290448d8638379fcc0f3648248d6ab54f1e1fc
agouliel <agouliel@gmail.com>
2026-01-22 17:15:13 +0200

    added sockets

---
+++ sockets/udpclient.py
@@ -0,0 +1,16 @@
+import socket as s
+
+serverName = '127.0.0.1'
+serverPort = 10000
+
+clientSocket = s.socket(s.AF_INET, s.SOCK_DGRAM)
+#clientSocket.connect((serverName, serverPort))
+
+message = 'test'
+
+clientSocket.sendto(message.encode('utf-8'), (serverName, serverPort))
+
+modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
+print(modifiedMessage)
+
+clientSocket.close()
---
+++ sockets/udpserver.py
@@ -0,0 +1,11 @@
+import socket as s
+
+serverPort = 10000
+
+serverSocket = s.socket(s.AF_INET, s.SOCK_DGRAM)
+serverSocket.bind(('', serverPort))
+
+while 1:
+  message, clientAddress = serverSocket.recvfrom(2048)
+  modifiedMessage = message.upper()
+  serverSocket.sendto(modifiedMessage, clientAddress)
