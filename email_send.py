9f3e8356f741043cbebabbc263217ab0b2ad20c9
agouliel <agouliel@gmail.com>
2026-03-28 22:08:49 +0000

    fixed email_send.py

--- email_send.py
+++ email_send.py
@@ -1,7 +1,7 @@
 import smtplib
 from email.mime.text import MIMEText
 
-sender = 'alex@goulielmos@org'  # your sending address
+sender = 'alex@goulielmos.org'  # your sending address
 recipients = 'bracts.teacups-09@icloud.com'  # comma separated list of recipients
 msg = MIMEText("""Hello""")
 
