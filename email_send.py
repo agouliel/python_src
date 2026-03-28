314b67d808f0516c853f5876cc357bce31a20b1a
agouliel <agouliel@gmail.com>
2026-03-28 17:27:55 +0000

    added email_send

---
+++ email_send.py
@@ -0,0 +1,15 @@
+import smtplib
+from email.mime.text import MIMEText
+
+sender = 'alex@goulielmos@org'  # your sending address
+recipients = 'bracts.teacups-09@icloud.com'  # comma separated list of recipients
+msg = MIMEText("""Hello""")
+
+msg['Subject'] = 'Test email'
+msg['From'] = sender
+msg['To'] = recipients.replace(',', ';')
+
+s = smtplib.SMTP('localhost', 1587)
+s.sendmail(sender, recipients.split(','), msg.as_string())
+s.quit()
+
